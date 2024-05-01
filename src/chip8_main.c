ecd77d376c63ac1aa7b78415638797463b6f3dc0
ThestralWarrior <38424304+ThestralWarrior@users.noreply.github.com>
2024-05-01 21:29:24 +0530

    upgraded to load and run ch8 files

--- src/chip8_main.c
+++ src/chip8_main.c
@@ -2,28 +2,45 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
 #include "chip8_core.h"
 #include "chip8_platform.h"
+
+struct timeval start_time, end_time;
 
 int main(int argc, char **argv) {
     printf("%d\n", argc);
     printf("%s\n", argv[0]);
+    char rom[100] = "roms/test-suite/";
+    char *file;
+    if(argc != 2) {
+        printf("Not enough arguments. Passing IBM.ch8 as program.\n");
+        file = "IBM.ch8";
+    } else {
+        file = argv[1];
+    }
+
+    printf("Loading rom: %s\n", rom);
+    strcat(rom, file);
+    printf("%s\n", rom);
     if(!init()) {
         fprintf(stderr, "Error while initializing.\n");
         exit(EXIT_FAILURE);
     }
     if(!stateinit()) exit(EXIT_FAILURE);
-    init_cpu();
-    load_rom("rom/IBM.ch8");
-    print_rom_instructions("rom/IBM.ch8");
+    initializeCPU();
+    loadROM(rom);
+    printInstructions(rom);
     clearscreen();
     while(state != QUIT) {
-    //    printf("Awaiting input\n");
         handleinputs();
-    //    printf("Delay started\n");
-        emulate_cycle();
-        SDL_Delay(16);
-    //    printf("Updating screen\n");
+        if(state == PAUSED) continue;
+        gettimeofday(&start_time, NULL);
+        executeInstruction();
+        gettimeofday(&end_time, NULL);
+        uint32_t timeInMillis = (uint32_t)((double)(end_time.tv_sec - start_time.tv_sec) + (double)(end_time.tv_usec - start_time.tv_usec) / 1000000.0) * 1000;
+        SDL_Delay(16 - timeInMillis);
         updatescreen();
     }
     cleanup();
